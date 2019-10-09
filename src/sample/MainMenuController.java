766ae10c45c81ad35ac64e506e332408d037a877
MKTPR <marickimtpr@gmail.com>
2019-10-09 23:51:11 +1300

    "Finished basics of ManageCreationGUI and the controller"

--- src/sample/MainMenuController.java
+++ src/sample/MainMenuController.java
@@ -1,7 +1,14 @@
 package sample;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class MainMenuController {
 
@@ -9,11 +16,19 @@ public class MainMenuController {
     @FXML private Button _manageCreationButton;
     @FXML private Button _playGameButton;
 
-    public void createCreationOnClick() {
-        System.out.println("yo");
-    }
-    public void manageCreationOnClick() {
-        System.out.println("yo");
+    public void createCreationOnClick() { System.out.println("yo"); }
+
+    @FXML public void manageCreationOnClick() throws IOException {
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(Main.class.getResource("../scene/ManageCreation.fxml"));
+        Parent layout = loader.load();
+
+        Scene scene = new Scene(layout);
+        Stage Stage = new Stage();
+        Stage.setScene(scene);
+        //We can exclude the code below for multi-purpose/concurrency
+        Stage.initModality(Modality.APPLICATION_MODAL);
+        Stage.showAndWait();
     }
     public void playGameOnClick() {
         System.out.println("yo");
---
+++ src/sample/ManageCreationController.java
@@ -0,0 +1,29 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class ManageCreationController {
+
+    @FXML private Button _deleteCreationButton;
+    @FXML private Button _playCreationButton;
+    @FXML private Button _playButton;
+    @FXML private Button _pauseButton;
+    @FXML private Button _fastPlayButton;
+    @FXML private Button _slowPlayButton;
+
+
+    @FXML public void deleteCreationOnClick(ActionEvent actionEvent) {
+    }
+    @FXML public void playCreationOnClick(ActionEvent actionEvent) {
+    }
+    @FXML public void playMediaOnClick(ActionEvent actionEvent) {
+    }
+    @FXML public void pauseMediaOnClick(ActionEvent actionEvent) {
+    }
+    @FXML public void fastPlayMediaOnClick(ActionEvent actionEvent) {
+    }
+    @FXML public void slowPlayMediaOnClick(ActionEvent actionEvent) {
+    }
+}
