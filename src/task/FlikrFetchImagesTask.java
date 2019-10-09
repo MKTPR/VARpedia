564c0f2cdf8caba01291ee640d8a51ebaf3d5bb2
dy5es41 <dy5es41@protonmail.com>
2019-10-09 21:38:56 +1300

    Flickr task not implemented

---
+++ src/task/FlikrFetchImagesTask.java
@@ -0,0 +1,11 @@
+package task;
+
+import javafx.concurrent.Task;
+
+public class FlikrFetchImagesTask extends Task<Void> {
+
+    @Override
+    protected Void call() throws Exception {
+        return null;
+    }
+}
