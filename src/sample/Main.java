f8d2298e786eeaa7562ee99c487c45ae0a9392d9
dy5es41 <dy5es41@protonmail.com>
2019-10-09 13:17:38 +1300

    Changed file name format to be more consistent, added MainMenu function stubs and added ManageCreation fxml stub

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,10 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.image.Image;
 import javafx.stage.Stage;
-
-import java.io.File;
 
 
 public class Main extends Application {
@@ -25,7 +22,7 @@ public class Main extends Application {
         fs.createCreationsDirectory();
 
         // Load GUI
-        Parent root = FXMLLoader.load(getClass().getResource("../scene/MainWindow.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("../scene/MainMenu.fxml"));
         primaryStage.setTitle(applicationName);
 
         //File logo = new File("../../resources/logo.svg");
---
+++ src/sample/MainMenuController.java
@@ -0,0 +1,25 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class MainMenuController {
+
+    @FXML private Button _createCreationButton;
+    @FXML private Button _manageCreationButton;
+    @FXML private Button _playGameButton;
+
+    public void createCreationOnClick() {
+        System.out.println("yo");
+    }
+    public void manageCreationOnClick() {
+        System.out.println("yo");
+    }
+    public void playGameOnClick() {
+        System.out.println("yo");
+    }
+    public void helpOnClick() {
+        System.out.println("yo");
+    }
+
+}
