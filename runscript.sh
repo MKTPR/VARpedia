2b15d753bca3e8890235668548a3fac31a5b1d2f
dy5es41 <dy5es41@protonmail.com>
2019-10-14 01:34:14 +1300

    update script

--- runscript.sh
+++ runscript.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo apt install openjdk-11-jdk
 sudo update-java-alternatives -s java-1.11.0-openjdk-amd64
-/usr/bin/java --module-path /home/student/Downloads/openjfx-13-rc+2_linux-x64_bin-sdk/javafx-sdk-13/lib --add-modules javafx.base,javafx.controls,javafx.media,javafx.graphics,javafx.fxml -jar pedia.jar
+/usr/bin/java --module-path /home/student/Downloads/openjfx-13-rc+2_linux-x64_bin-sdk/javafx-sdk-13/lib --add-modules javafx.base,javafx.controls,javafx.media,javafx.graphics,javafx.fxml -jar VARpedia-release.jar
 
 
 
